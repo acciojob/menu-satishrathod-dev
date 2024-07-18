752929883c2bba5791b26ec008e0d8f67b95de47
Satish Rathod <satishrathod.dev@gmail.com>
2024-07-18 12:16:29 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,36 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import { items } from "./Data";
+import Categories from "./Categories";
+import Menu from "./Menu";
+import "./App.css";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
