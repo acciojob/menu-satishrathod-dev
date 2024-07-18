42862ef021dbfb22024bfb7857ad25dd5a2458a2
Satish Rathod <satishrathod.dev@gmail.com>
2024-07-19 00:05:07 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,56 @@
-import React, { useEffect, useState } from "react";
-import { items } from "./Data";
-import Categories from "./Categories";
-import Menu from "./Menu";
-import "./App.css";
+// import React, { useEffect, useState } from "react";
+// import { items } from "./Data";
+// import Categories from "./Categories";
+// import Menu from "./Menu";
+// import "./App.css";
 
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+// const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+// function App() {
+//   const [menuItems, setMenuItems] = useState(items);
+//   //   console.log(menuItems);
+//   const [categories, setCategories] = useState(allCategories);
+
+//   const filterItems = (category) => {
+//     if (category === "all") {
+//       setMenuItems(items);
+//       return;
+//     }
+//     const newItems = items.filter((item) => item.category === category);
+//     setMenuItems(newItems);
+//   };
+
+//   //   useEffect(() => {
+//   //     async function getData() {
+//   //       let d = await items;
+//   //       setMenuItems([d]);
+//   //     }
+//   //   }, []);
+
+//   return (
+//     <main id="main">
+//       <section className="menu section">
+//         <div className="title">
+//           <h2>Our Menu</h2>
+//           <div className="underline"></div>
+//         </div>
+//         <Categories categories={categories} filterItems={filterItems} />
+//         <Menu items={menuItems} />
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import Menu from "./Menu.js";
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  //   console.log(menuItems);
-  const [categories, setCategories] = useState(allCategories);
-
-  const filterItems = (category) => {
-    if (category === "all") {
-      setMenuItems(items);
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
-
-  //   useEffect(() => {
-  //     async function getData() {
-  //       let d = await items;
-  //       setMenuItems([d]);
-  //     }
-  //   }, []);
-
   return (
-    <main id="main">
-      <section className="menu section">
-        <div className="title">
-          <h2>Our Menu</h2>
-          <div className="underline"></div>
-        </div>
-        <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
-      </section>
-    </main>
+    <div>
+      <Menu />
+    </div>
   );
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,96 @@
-import React from "react";
-import MenuItem from "./MenuItem";
+import React, { useState } from "react";
+import { items as data } from "./Data";
+// import MenuItem from "./MenuItem";
 
-const Menu = ({ items }) => {
+// const Menu = ({ items }) => {
+//   return (
+//     <div>
+//       <div className="section-center">
+//         {items.map((menuItem, index) => {
+//           return <MenuItem menuItem={menuItem} key={index} />;
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Menu;
+
+const Menu = () => {
+  let [dishes, setDishes] = useState(data);
+
+  function handleCategories(e) {
+    if (e.target.innerText === "All") {
+      setDishes(data);
+      return;
+    }
+    if (e.target.innerText === "Breakfast") {
+      setDishes(data.filter((food) => food.category === "breakfast"));
+      return;
+    }
+    if (e.target.innerText === "Lunch") {
+      setDishes(data.filter((food) => food.category === "lunch"));
+      return;
+    }
+    if (e.target.innerText === "Shakes") {
+      setDishes(data.filter((food) => food.category === "shakes"));
+      return;
+    }
+  }
+
   return (
-    <div>
-      <div className="section-center">
-        {items.map((menuItem, index) => {
-          return <MenuItem menuItem={menuItem} key={index} />;
-        })}
-      </div>
+    <div id="main">
+      <h1>Our Menu</h1>
+      <p onClick={handleCategories}>All</p>
+      <p id="filter-btn-1" onClick={handleCategories}>
+        Breakfast
+      </p>
+      <p id="filter-btn-2" onClick={handleCategories}>
+        Lunch
+      </p>
+      <p id="filter-btn-3" onClick={handleCategories}>
+        Shakes
+      </p>
+
+      {dishes.map((food) =>
+        food.category == "shakes" ? (
+          <div data-test-id="menu-item-shakes">
+            <img src={food.img} alt={food.title}></img>
+            <h2>
+              <span>{food.title}</span>{" "}
+              <span className="price">{food.price}</span>
+            </h2>
+            <p>{food.desc}</p>
+          </div>
+        ) : food.category == "lunch" ? (
+          <div data-test-id="menu-item-lunch">
+            <img src={food.img} alt={food.title}></img>
+            <h2>
+              <span>{food.title}</span>{" "}
+              <span className="price">{food.price}</span>
+            </h2>
+            <p>{food.desc}</p>
+          </div>
+        ) : food.category == "breakfast" ? (
+          <div data-test-id="menu-item-breakfast">
+            <img src={food.img} alt={food.title}></img>
+            <h2>
+              <span>{food.title}</span>{" "}
+              <span className="price">{food.price}</span>
+            </h2>
+            <p>{food.desc}</p>
+          </div>
+        ) : (
+          <div>
+            <img src={food.img} alt={food.title}></img>
+            <h2>
+              <span>{food.title}</span>{" "}
+              <span className="price">{food.price}</span>
+            </h2>
+            <p>{food.desc}</p>
+          </div>
+        )
+      )}
     </div>
   );
 };
